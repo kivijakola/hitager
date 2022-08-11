1926426ff52c298361fe54ee0cb0ecac30a44717
Philipp <philipp.springer@googlemail.com>
2022-08-11 23:43:49 +0200

    Ensure proper reading of BMW remote page (Pg15) for different key variants

--- sources/Hitager/Properties/AssemblyInfo.cs
+++ sources/Hitager/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Resources;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
-[assembly: AssemblyVersion("1.3.1.*")]
+[assembly: AssemblyVersion("1.3.2.*")]
 
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
